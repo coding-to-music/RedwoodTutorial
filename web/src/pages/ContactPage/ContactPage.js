f239015ce98efe431755b25c280c40cdc2c3c0dd
coding-to-music <connors.tom@gmail.com>
2020-09-30 00:06:32 -0400

    added Contact Page

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,14 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const ContactPage = () => {
+  return (
+    <BlogLayout>
+      <h1>ContactPage</h1>
+      <p>Find me in ./web/src/pages/ContactPage/ContactPage.js</p>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
+
+// import { Link, routes } from '@redwoodjs/router'
