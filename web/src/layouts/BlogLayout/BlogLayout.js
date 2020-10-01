922b3b32d06fc66f3d6a16329f7f29d38e3c8204
coding-to-music <connors.tom@gmail.com>
2020-10-01 04:15:16 -0400

    removed the p from wrapping children in blog layout

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -18,28 +18,9 @@ const BlogLayout = ({ children }) => {
           </ul>
         </nav>
       </header>
-
-      <main>
-        <p>{children}</p>
-      </main>
+      <main>{children}</main>
     </>
   )
 }
 
 export default BlogLayout
-
-{
-  /*
-
-<p>dev.to/codingtomusic</p>
-<p>twitter.com/ThomasConnors</p>
-<p>github.com/coding-to-music</p>
-<p>
-  Find me in <tt>./web/src/pages/HomePage/HomePage.js</tt>
-</p>
-<p>
-  My default route is named <tt>home</tt>, link to me with `
-  <Link to={routes.home()}>Home</Link>`
-</p>
-*/
-}
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -2,9 +2,13 @@ import BlogLayout from 'src/layouts/BlogLayout'
 import { Form, Label, TextField, Submit } from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <BlogLayout>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Label name="name" />
         <TextField name="name" />
         <Submit>Save</Submit>
