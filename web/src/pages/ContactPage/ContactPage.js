2ae4b2f15d7c7f28d31ef0ea44dd548d8b3b33ea
coding-to-music <connors.tom@gmail.com>
2020-10-03 03:58:42 -0400

    forms

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,16 +1,54 @@
 import BlogLayout from 'src/layouts/BlogLayout'
-import { Form, Label, TextField, Submit } from '@redwoodjs/forms'
+import {
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  FieldError,
+  Submit,
+} from '@redwoodjs/forms'
+// import { Redirect } from '@redwoodjs/router'
 
 const ContactPage = () => {
   const onSubmit = (data) => {
     console.log(data)
+    console.log(data.name)
+    console.log(data.email)
+    console.log(data.message)
   }
+
+  // input.error, textArea.error {
+  //   border-color: red;
+  //   color: red;
+  // }
+
+  // Label.error {
+  //   color: red;
+  // }
 
   return (
     <BlogLayout>
+      <h1>Contact Page</h1>
+      <p>Tell me about my things!</p>
       <Form onSubmit={onSubmit}>
         <Label name="name" />
-        <TextField name="name" />
+        <TextField
+          name="name"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <Label name="email" />
+        <TextField
+          name="email"
+          errorClassName="error"
+          validation={{ required: true, pattern: { value: /[^@]+@[^.]+\..+/ } }}
+        />
+        <Label name="message" />
+        <TextAreaField
+          name="message"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
         <Submit>Save</Submit>
       </Form>
     </BlogLayout>
